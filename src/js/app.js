1e697dd628f92798a258bbf914d92a8fb2c05805
Witold Pruchnicki <witekpruchnicki@gmail.com>
2021-07-28 07:27:42 +0200

    First initial commit

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+import '../scss/main.scss';
+require.context('../images', true, /\.(jpe?g|png|gif|svg)$/i);
+
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.querySelector('.nav__menu');
+const menuBar = document.querySelector('.nav_toggle')
+
+menuBar.addEventListener('click', ()=>{
+    navMenu.classList.toggle('nav__menu-close')
+})
+
+/*==================== ACCORDION SKILLS ====================*/
+
+/*==================== QUALIFICATION TABS ====================*/
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+/*==================== TESTIMONIAL ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.min.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    watch: false,
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [['autoprefixer']],
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+
+            {
+                test: /\.(png|jpe?g|gif|webp|awif|svg)$/i,
+                loader: 'url-loader',
+                options: {
+                    name: 'images/[hash]-[name].[ext]',
+                    publicPath: 'src',
+                },
+            },
+            {
+                test: /\.(png|jpg|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'src/images/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            filename: 'index.html',
+            template: 'src/index.html',
+        }),
+    ],
+};
